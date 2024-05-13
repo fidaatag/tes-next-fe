db3ea193dff251c2e99072eded21e3cf5bae2f15
christianjoshua <Razor091203@gmail.com>
2024-05-13 15:17:50 +0700

    landing page

---
+++ src/app/page.tsx
@@ -0,0 +1,21 @@
+import { Card } from "@/src/components/ui/card";
+import Image from "next/image";
+import Hero from "@/src/components/hero";
+import WhyUs from "@/src/components/WhyUs";
+import AllClass from "@/src/components/AllClass";
+import Partner from "@/src/components/Partner";
+import Subscribe from "@/src/components/Subscribe";
+import Footer from "@/src/components/layout/footer";
+
+export default function Home() {
+  return (
+    <main >
+      <Hero />
+      <WhyUs />
+      <AllClass />
+      <Partner />
+      <Subscribe />
+      <Footer />
+    </main>
+  );
+}
---
+++ src/components/Partner.tsx
@@ -0,0 +1,70 @@
+import React from "react";
+import Image from "next/image";
+
+function Partner() {
+  return (
+    <div className="bg-gray-100 py-4">
+      <h2 className="text-center text-xl md:text-2xl font-bold mb-4 text-blue-400">
+        Kerjasama Internasional
+      </h2>
+      <div className="flex flex-wrap justify-center items-center gap-2 px-2">
+        <div className="w-1/3 sm:w-1/4 md:w-1/6">
+          <Image
+            src="/partner/kerjasama1.png"
+            alt="KDU University"
+            width={160}
+            height={90}
+            layout="responsive"
+          />
+        </div>
+        <div className="w-1/3 sm:w-1/4 md:w-1/6">
+          <Image
+            src="/partner/kerjasama2.png"
+            alt="European University of Applied Sciences"
+            width={160}
+            height={90}
+            layout="responsive"
+          />
+        </div>
+        <div className="w-1/3 sm:w-1/5 md:w-1/6">
+          <Image
+            src="/partner/kerjasama3.png"
+            alt="University of King Saud"
+            width={160}
+            height={90}
+            layout="responsive"
+          />
+        </div>
+        <div className="w-1/3 sm:w-1/4 md:w-1/6">
+          <Image
+            src="/partner/kerjasama4.png"
+            alt="Other University"
+            width={160}
+            height={90}
+            layout="responsive"
+          />
+        </div>
+        <div className="w-1/3 sm:w-1/4 md:w-1/6">
+          <Image
+            src="/partner/kerjasama5.png"
+            alt="Another University"
+            width={160}
+            height={90}
+            layout="responsive"
+          />
+        </div>
+        <div className="w-1/3 sm:w-1/4 md:w-1/6">
+          <Image
+            src="/partner/kerjasama6.png"
+            alt="Last University"
+            width={160}
+            height={90}
+            layout="responsive"
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Partner;
