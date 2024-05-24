c4bf412625090145a43db4f7a55315cb0623c7e4
fidaatag <fidaa2fidaa@gmail.com>
2024-05-24 11:01:40 +0700

    pecah kode, + skema klik 1x hit api editkelas-buatbab

--- src/app/dashboard/kelas/[slug]/edit/page.tsx
+++ src/app/dashboard/kelas/[slug]/edit/page.tsx
@@ -3,106 +3,95 @@
 import React, { useEffect, useState } from "react";
 import { Button } from "@/src/components/ui/button";
 import FormKelas from "@/src/components/FormKelas";
-import axios from 'axios';
 import { ListKelas } from "@/src/types";
-
+import { APIDetailKelas, APIEditKelas } from "@/src/service/ApiKelas";
+import { APIBuatBab } from "@/src/service/ApiBab";
 
 type PageEditKelasProps = {
-  params:  {slug: string}
-}
+  params: { slug: string };
+};
 
-const PageEdit = ({params} : PageEditKelasProps) => {
-
-  const id = params.slug[0]
-
-  const token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOi8vMTI3LjAuMC4xOjgwMDAvYXBpL2xvZ2luIiwiaWF0IjoxNzE2MjgxOTAzLCJleHAiOjE3MTYyODU1MDMsIm5iZiI6MTcxNjI4MTkwMywianRpIjoiOEFzSEtRdGpjU0w5N1NFYSIsInN1YiI6IjEiLCJwcnYiOiIyM2JkNWM4OTQ5ZjYwMGFkYjM5ZTcwMWM0MDA4NzJkYjdhNTk3NmY3IiwidXNlciI6eyJpZCI6MSwiZnVsbF9uYW1lIjoiYWRtaW4iLCJ1c2VybmFtZSI6ImFkbWluIiwiZW1haWwiOiJhZG1pbkBvcGVuY291cnNlLmNvbSIsInBob25lX251bWJlciI6IjA4MTIzNDU2Nzg5IiwiYmlvIjoiYWRtaW4iLCJwcm9maWxlX3BpY3R1cmUiOiIiLCJlbWFpbF92ZXJpZmllZF9hdCI6bnVsbCwiY3JlYXRlZF9hdCI6IjIwMjQtMDUtMTZUMDQ6NTc6MDMuMDAwMDAwWiIsInVwZGF0ZWRfYXQiOiIyMDI0LTA1LTE2VDA0OjU3OjAzLjAwMDAwMFoiLCJjcmVhdGVkX2J5IjpudWxsLCJ1cGRhdGVkX2J5IjpudWxsLCJyb2xlIjoiYWRtaW4iLCJkZWxldGVkX2F0IjpudWxsfX0.TC9LUyoBFjuKOdWDd0X7JQDwb7s5ONqrneSWFWoTC6o"
-
-  const [typeButtonForm, setTypeButtonForm] = useState("")
-
+const PageEdit = ({ params }: PageEditKelasProps) => {
+  const id = params.slug;
+  const [typeButtonForm, setTypeButtonForm] = useState("");
   const [data_OldForm, set_DataOldForm] = useState<ListKelas>();
+  const [responForm, setResponForm] = useState();
+  const [dataAllBab, setDataAllBab] = useState<[]>(); // ambil data bab yang tersedia selama pengisian form
 
-  const [dataFormKelas, setDataFormKelas] = useState({
-    nama_kelas: '',
-    tentang_kelas: '',
-    lama_kelas: '',
-    upaya: '',
-    harga_kelas: '',
-    bahasa_kelas: '',
-    transkrip_video: ''
-  });
-
-  // mendapatkan value dari komponen form
-  const handleAllValueForm = (e :any) => { setDataFormKelas(e) }
-
-  // memastikan data form terisi
-  const isDataFormKelas_Filled = () => {
-    const allPropsEmpty = Object.values(dataFormKelas).every(value => value === '');
-    return !allPropsEmpty;
+  // ambil data lama buat nampilin di versi edit
+  const dataLamaKelas = async () => {
+    const respon = await APIDetailKelas(id);
+    set_DataOldForm(respon);
   };
-    
 
-  // get old data
+  // trigger ambil data lama saat pertama kali reload / id berubah
   useEffect(() => {
-    axios.get(`http://localhost:8000/api/admin/courses/${id}`, {
-      headers: {"Authorization" : `Bearer ${token}`}
-    })
-    .then(response => {
-      // console.log(response)
-      set_DataOldForm(response.data.data)
-    })
-    .catch(error => {
-      console.log(error)
-    })
-  }, [])
+    dataLamaKelas();
+  }, [id]);
 
-  // update course
-  useEffect(() => {
-    if (isDataFormKelas_Filled()) {
-      axios.put(`http://localhost:8000/api/admin/courses/${id}`, {
-        name: dataFormKelas.nama_kelas,
-        description: dataFormKelas.tentang_kelas,
-        about: dataFormKelas.tentang_kelas,
-        duration: dataFormKelas.lama_kelas,
-        effort_taken: dataFormKelas.upaya,
-        status: "drafted",
-        price: dataFormKelas.harga_kelas,
-        language: dataFormKelas.bahasa_kelas,
-        image_cover: "default_image_cover",
-        created_by: 40,
-        updated_by: 9, 
-        course_category_id: 1,
-        is_superior: true
-      }, {
-        headers: {"Authorization" : `Bearer ${token}`}
-      })
-      .then(response => {
-        console.log("Data posted successfully:", response);
-      })
-      .catch(error => {
-        console.error("Error posting data:", error);
-      });
+  const handleBtnSubmit = async (e: string) => {
+    setTypeButtonForm(e);
+
+    if (typeButtonForm === "drafted") {
+      const respon = await APIEditKelas(responForm, typeButtonForm, id);
+
+      if (respon?.course?.id) {
+        const course_id = respon.course.id;
+
+        // hit api buat bab, jika ada bab yang terbaru
+        dataAllBab?.map((itemBab: any) => {
+          if (!itemBab.id) {
+            return APIBuatBab(itemBab, course_id);
+          }
+        });
+      }
     }
-  }, [isDataFormKelas_Filled(), dataFormKelas])
 
-  
+    if (typeButtonForm === "published") {
+      const respon = await APIEditKelas(responForm, typeButtonForm, id);
+      alert(JSON.stringify(respon));
+    }
+
+    if (typeButtonForm === "hapus") {
+      const respon = await APIEditKelas(responForm, typeButtonForm, id);
+      alert(JSON.stringify(respon));
+    }
+  };
 
   return (
     <>
       <div className="p-3">
         <div className="flex justify-between mb-10 items-center">
-          <h1 className="text-xl font-bold">Tambah Kelas</h1>
+          <h1 className="text-xl font-bold">Edit Kelas</h1>
 
           <div className="flex gap-2">
-            <Button form={typeButtonForm} onClick={() => setTypeButtonForm("hapus")} variant="link">Hapus Kelas</Button>
-            <Button form={typeButtonForm} onClick={() => setTypeButtonForm("draf")} variant="outline">Simpan Draf</Button>
-            <Button form={typeButtonForm} onClick={() => setTypeButtonForm("publish")} variant="secondary">Publikasikan</Button>
+            <Button onClick={() => handleBtnSubmit("hapus")} variant="link">
+              Hapus Kelas
+            </Button>
+            <Button
+              form={typeButtonForm}
+              onClick={() => handleBtnSubmit("drafted")}
+              variant="outline"
+            >
+              Simpan Draf
+            </Button>
+            <Button
+              form={typeButtonForm}
+              onClick={() => handleBtnSubmit("published")}
+              variant="secondary"
+            >
+              Publikasikan
+            </Button>
           </div>
         </div>
 
         <div className="w-full">
-          
-          <FormKelas typeBtn={typeButtonForm} AllValue={handleAllValueForm} oldData={data_OldForm}/>
-
+          <FormKelas
+            typeBtn={typeButtonForm}
+            oldData={data_OldForm}
+            respon={(e: any) => setResponForm(e)}
+            dataBab={(e: any) => setDataAllBab(e)}
+          />
         </div>
       </div>
     </>
