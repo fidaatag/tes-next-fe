31fd23afd97513cd8d51d4838eb6422bfa6cf8bb
fidaatag <fidaa2fidaa@gmail.com>
2024-05-27 11:42:21 +0700

    tambah addBab ketika di page tambah kelas

--- src/app/dashboard/kelas/tambah/page.tsx
+++ src/app/dashboard/kelas/tambah/page.tsx
@@ -1,23 +1,37 @@
 "use client"
 
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { Button } from "@/src/components/ui/button";
 import FormKelas from "@/src/components/FormKelas";
 import { APIBuatKelas } from "@/src/service/ApiKelas";
+import { APIBuatBab } from "@/src/service/ApiBab";
 
 const TambahPage = () => {
   const [typeButtonForm, setTypeButtonForm] = useState("");
   const [responForm, setResponForm] = useState();
+  const [dataAllBab, setDataAllBab] = useState<[]>();
 
-  const handleBtnSubmit = async (e: string) => {
+
+  const handleBtnSubmit = async (e: string, event: React.FormEvent) => {
+    event.preventDefault() // cegah page biar ga reload langsung, tunggu sampe bab selesai
     setTypeButtonForm(e);
 
-    if (typeButtonForm === "drafted") {
-      const respon = await APIBuatKelas(responForm, typeButtonForm);
-      alert(JSON.stringify(respon));
+    if (e === "drafted") {
+    const respon = await APIBuatKelas(responForm, e);
+
+      if (respon?.course?.id) {
+        const course_id = respon.course.id;
+
+        // hit api buat bab, jika ada bab yang terbaru
+        dataAllBab?.map((itemBab: any) => {
+          if (!itemBab.id) {
+            return APIBuatBab(itemBab, course_id);
+          }
+        });
+      }
     }
 
-    if (typeButtonForm === "published") {
+    if (e === "published") {
       const respon = await APIBuatKelas(responForm, typeButtonForm);
       alert(JSON.stringify(respon));
     }
@@ -32,14 +46,14 @@ const TambahPage = () => {
           <div className="flex gap-2">
             <Button
               form={typeButtonForm}
-              onClick={() => handleBtnSubmit("drafted")}
+              onClick={(event) => handleBtnSubmit("drafted", event)}
               variant="outline"
             >
               Simpan Draf
             </Button>
             <Button
               form={typeButtonForm}
-              onClick={() => handleBtnSubmit("published")}
+              onClick={(event) => handleBtnSubmit("published", event)}
               variant="secondary"
             >
               Publikasikan
@@ -51,6 +65,7 @@ const TambahPage = () => {
           <FormKelas
             typeBtn={typeButtonForm}
             respon={(e: any) => setResponForm(e)}
+            dataBab={(e: any) => setDataAllBab(e)}
           />
         </div>
       </div>
