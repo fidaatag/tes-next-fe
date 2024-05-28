7d97969261d425cb06db855642ee638a626e76c7
fidaatag <fidaa2fidaa@gmail.com>
2024-05-28 15:02:21 +0700

    mengedit materi bab + api

--- src/app/dashboard/kelas/[slug]/edit/[bab]/[materi]/page.tsx
+++ src/app/dashboard/kelas/[slug]/edit/[bab]/[materi]/page.tsx
@@ -1,10 +1,97 @@
-import React from 'react'
+"use client"
 
+import { Button } from "@/src/components/ui/button";
+import { Input } from "@/src/components/ui/input";
+import { APIDetailMateri, APIEditMateri } from "@/src/service/ApiMateri";
+import React, { useEffect, useState } from "react";
+import dynamic from "next/dynamic";
 
-const page = () => {
-  return (
-    <div>page EDIT MATERI</div>
-  )
+type PageEditMateriProps = {
+  params: { materi: string }
 }
 
-export default page
+const TextEditor = dynamic(() => import("./TextEditor"), { ssr: false });
+
+const pageEditMateri = ({params} : PageEditMateriProps) => {
+  const module_id = params.materi;
+
+  const dataLamaMateri = async () => {
+    const respon = await APIDetailMateri(module_id);
+    console.log(respon)
+    setDataMateri({
+      module_title: respon.module_title,
+      description: respon.description
+    });
+  }
+
+  useEffect(() => {
+    dataLamaMateri();
+  }, [module_id]);
+
+  const [dataMateri, setDataMateri] = useState({
+    module_title: "",
+    description: "",
+  });
+
+  const handleSubmitMateri = async () => {
+    const respon = await APIEditMateri(dataMateri, module_id);
+    console.log(respon);
+  };
+
+  console.log(dataMateri)
+
+  return (
+    <>
+      <div>
+        <div className="flex justify-between items-center p-4">
+          <h1 className="text-xl font-bold">Tambah Kelas</h1>
+
+          <Button variant="secondary" onClick={() => handleSubmitMateri()}>
+            Simpan
+          </Button>
+        </div>
+
+        <hr />
+
+        <div className="w-full grid grid-cols-4 justify-between">
+          {/* // ! --- sisi kanan ----  */}
+          <div className="col-span-3 flex flex-col gap-4 px-4 pt-4">
+            <Input
+              placeholder="Judul Materi"
+              className="w-full mb-3"
+              onChange={(e) =>
+                setDataMateri({ ...dataMateri, module_title: e.target.value })
+              }
+              value={dataMateri?.module_title}
+            />
+            <TextEditor
+              valueText={(e: any) =>
+                setDataMateri({ ...dataMateri, description: e })
+              }
+              value={dataMateri?.description}
+            />
+          </div>
+
+          {/* // ! --- sisi kiri ----  */}
+          <div className="border">
+            <h1 className="text-center text-md font-bold py-3 bg-gray-400">
+              Informasi kelas
+            </h1>
+
+            <div>
+              <div className="bg-gray-300">
+                <h2 className="py-2 pl-2 ">Durasi Kelas</h2>
+              </div>
+              <div className="p-2">
+                <div className="w-full h-40 bg-slate-200 mb-3"></div>
+                <Input disabled value="https://youtube.lalala.com" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default pageEditMateri;
--- src/service/ApiMateri.tsx
+++ src/service/ApiMateri.tsx
@@ -28,3 +28,44 @@ export const APIBuatMateri = async (formData: any, sections_id: any): Promise<an
     throw error;
   }
 };
+
+export const APIEditMateri = async (formData: any, module_id: any): Promise<any> => {
+  const dataUpdateMateri = {
+    module_title: formData?.module_title,
+    description: formData?.description,
+    status: "finished",
+    created_by: 23,
+    updated_by: 43,
+
+  };
+
+  try {
+    const response = await axios.put(`http://localhost:8000/api/modules/${module_id}`, dataUpdateMateri,
+      // {
+      //   headers: { "Authorization": `Bearer ${token}` }
+      // }
+    );
+
+    return response.data;
+
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const APIDetailMateri = async (module_id: any): Promise<any> => {
+  try {
+    const response = await axios.get(`http://localhost:8000/api/modules/${module_id}`,
+      // {
+      //   headers: { "Authorization": `Bearer ${token}` }
+      // }
+    );
+
+    // console.log(response)
+    return response.data.module;
+
+  } catch (error) {
+    throw error;
+  }
+};
+
