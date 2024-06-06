37a6e29a82e4cecd38d7c495a99663d67acb2590
fidaatag <fidaa2fidaa@gmail.com>
2024-06-06 09:20:29 +0700

    ubah versi json jadi formData yang dynamic

--- src/service/ApiKelas.tsx
+++ src/service/ApiKelas.tsx
@@ -8,35 +8,102 @@ export const APIBuatKelas = async (
   formData: any,
   status: string
 ): Promise<any> => {
-  const dataCreateKelas = {
-    name: formData?.name,
-    description: formData?.description,
-    about: formData?.description,
-    duration: formData?.duration,
-    effort_taken: formData?.effort_taken,
-    status: status,
-    price: formData?.price,
-    language: formData?.language,
-    image_cover: "default_image_cover",
-    created_by: 40,
-    updated_by: 9,
-    course_category_id: formData?.course_category_id,
-    is_superior: true,
-  };
 
-  try {
-    const response = await axios.post(
-      "http://localhost:8000/api/admin/courses",
-      dataCreateKelas,
-      {
-        headers: { Authorization: `Bearer ${token}` },
-      }
-    );
+  const dataCreateKelas = new FormData();
 
-    return response.data;
-  } catch (error) {
-    return { error: true, message: "API Tidak Aktif" };
+  // Dynamically append all properties from formData
+  for (const key in formData) {
+    if (formData.hasOwnProperty(key)) {
+      const value = formData[key]
+      if (typeof value === "string" || value instanceof File) {
+        dataCreateKelas.append(key, value);
+
+      } else if (Array.isArray(value))
+        // dataCreateKelas.append(key, new Blob(value)); // jika valuenya berupa []
+        value.forEach((item) => {
+          dataCreateKelas.append(`${key}[]`, item);
+        });
+    }
   }
+
+  console.log(dataCreateKelas)
+
+
+
+  // const dataCreateKelas = {
+  //   name: formData?.name,
+  //   description: formData?.description,
+  //   about: formData?.description,
+  //   duration: formData?.duration,
+  //   effort_taken: formData?.effort_taken,
+  //   status: status,
+  //   price: formData?.price,
+  //   language: formData?.language,
+  //   image_cover: "default_image_cover",
+  //   created_by: 40,
+  //   updated_by: 9,
+  //   course_category_id: formData?.course_category_id,
+  //   is_superior: true,
+  // };
+
+//   interface Form {
+//     price?: string;
+//     description?: string;
+//     language?: string;
+//     transkrip_video?: string;
+// }
+
+//   const form: Form = {
+//     price: '58',
+//     description: 'Odio maxime natus qu',
+//     transkrip_video: 'english'
+//   };
+
+//   const dataCreateKelas = new FormData();
+// console.log(form)
+//   // formData.forEach((value: any, key: any) => {
+//   //   dataCreateKelas.append(key, value)
+//   // });
+
+//   for (const key in form ){
+//     dataCreateKelas.append(key, form[key as keyof Form])
+//   }
+
+//   // Object.keys(formData).forEach(key => {
+//   //   if (!dataCreateKelas.has(key)) {
+//   //     dataCreateKelas.append(key, formData[key]);
+//   //   }
+//   // });
+
+//   // Object.keys(formData).forEach(key => {
+//   //   if (Array.isArray(formData[key])) {
+//   //     formData[key].forEach((value: any, index: any) => {
+//   //       dataCreateKelas.append(`${key}[${index}]`, value);
+//   //     });
+//   //   } else if (typeof formData[key] === 'object') {
+//   //     dataCreateKelas.append(key, JSON.stringify(formData[key]));
+//   //   } else {
+//   //     dataCreateKelas.append(key, formData[key]);
+//   //   }
+//   // });
+
+//   // dataCreateKelas.append('nama', formData?.name)
+
+//   console.log(dataCreateKelas)
+
+  // try {
+  //   const response = await axios.post(
+  //     "http://localhost:8000/api/lecturer/courses",
+  //     dataCreateKelas,
+  //     {
+  //       headers: { Authorization: `Bearer ${token}` },
+  //     }
+  //   );
+
+  //   return response.data;
+  // } catch (error) {
+  //   return { error: true, message: "API Tidak Aktif" };
+  // }
 };
 
 export const APIEditKelas = async (
@@ -62,7 +129,7 @@ export const APIEditKelas = async (
 
   try {
     const response = await axios.put(
-      `http://localhost:8000/api/admin/courses/${id}`,
+      `http://localhost:8000/api/lecturer/courses/${id}`,
       dataUpdateKelas,
       {
         headers: { Authorization: `Bearer ${token}` },
@@ -75,10 +142,25 @@ export const APIEditKelas = async (
   }
 };
 
-export const APIDetailKelas = async (id: any) => {
+export const APIDetailKelasREAD = async (id: any) => {
   try {
     const response = await axios.get(
-      `http://localhost:8000/api/admin/courses/${id}`,
+      `http://localhost:8000/api/lecturer/courses/${id}/read`,
+      {
+        headers: { Authorization: `Bearer ${token}` },
+      }
+    );
+
+    return response.data;
+  } catch (error) {
+    return { error: true, message: "API Tidak Aktif" };
+  }
+};
+
+export const APIDetailKelasUPDATE = async (id: any) => {
+  try {
+    const response = await axios.get(
+      `http://localhost:8000/api/lecturer/courses/${id}/update`,
       {
         headers: { Authorization: `Bearer ${token}` },
       }
@@ -93,7 +175,7 @@ export const APIDetailKelas = async (id: any) => {
 export const APIHapusKelas = async (id: any) => {
   try {
     const response = await axios.delete(
-      `http://localhost:8000/api/admin/courses/${id}`,
+      `http://localhost:8000/api/lecturer/courses/${id}`,
       {
         headers: { Authorization: `Bearer ${token}` },
       }
@@ -108,7 +190,7 @@ export const APIHapusKelas = async (id: any) => {
 export const APISemuaKelas = async () => {
   try {
     const response = await axios.get(
-      `http://localhost:8000/api/admin/courses`,
+      `http://localhost:8000/api/lecturer/courses`,
       {
         headers: { Authorization: `Bearer ${token}` },
       }
